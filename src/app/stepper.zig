5f257a7c9123008f2a5de5a464f399d88771c744
Andy Armstrong <andy@hexten.net>
2026-04-21 22:23:50 +0100

    Maybe...

--- src/app/stepper.zig
+++ src/app/stepper.zig
@@ -13,12 +13,13 @@ const STSpin = @import("../drivers/STSpin.zig");
 
 pub const StepperController = struct {
     const Self = @This();
+
     pub const Config = struct {
         motor: *STSpin,
         min_rpm: f32,
         max_rpm: f32,
-        max_accel: f32, // rpm/s
-        max_decel: f32, // rpm/s
+        max_accel: f32, // rpm/m
+        max_decel: f32, // rpm/m
     };
 
     pub const State = enum { STOPPED, MOVING };
@@ -35,12 +36,19 @@ pub const StepperController = struct {
     last_tick: time.Absolute = .from_us(0),
 
     pub fn init(config: Config) Self {
+        assert(config.max_accel > 0);
+        assert(config.max_decel > 0);
+        assert(config.min_rpm > 0);
+        assert(config.max_rpm >= config.min_rpm);
+
         return .{ .config = config };
     }
 
     pub fn attach(self: *Self) void {
-        self.config.motor.state_ee.addListener(onStateChange, self);
-        self.config.motor.rt_ee.addListener(onRTStateChange, self);
+        const m = self.config.motor;
+        m.rt_ee.addListener(onRTStateChange, self);
+        m.setSpeed(0);
+        m.setRemaining(0);
     }
 
     fn adviseState(self: *Self, state: State) !void {
@@ -61,39 +69,52 @@ pub const StepperController = struct {
     }
 
     fn tick(self: *Self, now: time.Absolute) !void {
-        // const elapsed = now.diff(self.last_tick).to_us();
+        // Minutes because our accel / decel are in rpm/m
+        const elapsed: f32 = @as(f32, @floatFromInt(now.diff(self.last_tick).to_us())) /
+            1_000_000 / 60;
         self.last_tick = now;
 
-        const motor = self.config.motor;
-        // const dir = motor.direction.step();
-        // // Distance to destination; -ve means we're going the wrong way
-        // const to_dest = (self.set_point - motor.current_position) * dir;
+        const c = self.config;
+        const m = c.motor;
 
         // Current speed. Always +ve
-        const speed = motor.speed;
+        const speed = m.speed;
         assert(speed >= 0);
 
-        if (self.set_point == motor.current_position and speed <= self.config.min_rpm) {
-            motor.setRemaining(0);
-            motor.setSpeed(0);
+        // How many steps are we off?
+        const pos_error = self.set_point - m.current_position;
+
+        if (pos_error == 0 and speed <= c.min_rpm) {
+            // We've arrived
+            m.setSpeed(0);
+            m.setRemaining(0);
             try self.adviseState(.STOPPED);
             return;
         }
 
-        // if (to_dest < 0) {
-        //     // We've passed it; decelerate as hard as we can
-        //     const decel = @max(0, @as(f32, @floatCast(elapsed)) * self.config.max_decel);
-        //     _ = decel;
-        // } else {}
+        // If the motor knows which way it's going use that otherwise set the direction
+        // based on the direction we need to turn.
+        const dir = switch (m.direction.step()) {
+            -1, 1 => |d| d,
+            0 => std.math.sign(pos_error),
+            else => unreachable,
+        };
 
-        // const stopping_distance = (speed * speed) / 2 * self.config.max_decel;
-        // _ = stopping_distance;
-    }
+        // Distance to destination in revolutions; -ve means we're going the wrong way
+        const dest_dist = @as(f32, @floatFromInt(pos_error * dir)) /
+            @as(f32, @floatFromInt(m.stepsPerRevolution()));
+        // How far before we can stop?
+        const stop_dist = (speed * speed) / (2 * c.max_decel);
 
-    fn onStateChange(ctx: *anyopaque, e: STSpin.EventPayload) !void {
-        const self: *StepperController = @ptrCast(@alignCast(ctx));
-        _ = self;
-        _ = e;
+        if (dest_dist <= stop_dist) {
+            // Brake!
+            m.setSpeed(@max(0, speed - c.max_decel * elapsed));
+            m.setRemaing(dir * 2);
+        } else {
+            // Accelerate!
+            m.setSpeed(@max(c.min_rpm, @min(c.max_rpm, speed + c.max_accel * elapsed)));
+            m.setRemaining(std.math.sign(pos_error) * 2);
+        }
     }
 
     fn onRTStateChange(ctx: *anyopaque, e: STSpin.RTEventPayload) !void {
