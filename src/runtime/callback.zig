4c658e9782e74baf218eeb2a994534db97d7e1e4
Andy Armstrong <andy@hexten.net>
2026-04-21 14:10:15 +0100

    Explicit VoidCallback

--- src/runtime/callback.zig
+++ src/runtime/callback.zig
@@ -1,15 +1,16 @@
-pub fn makeCallback(comptime PayloadType: type) type {
-    if (@sizeOf(PayloadType) == 0) {
-        return struct {
-            const Self = @This();
-            context: *anyopaque,
-            handler: *const fn (ctx: *anyopaque) anyerror!void,
+pub const VoidCallback = struct {
+    const Self = @This();
+    context: *anyopaque,
+    handler: *const fn (ctx: *anyopaque) anyerror!void,
 
-            pub fn advise(self: Self) !void {
-                try self.handler(self.context);
-            }
-        };
+    pub fn advise(self: Self) !void {
+        try self.handler(self.context);
     }
+};
+
+pub fn makeCallback(comptime PayloadType: type) type {
+    if (@sizeOf(PayloadType) == 0)
+        return VoidCallback;
 
     return struct {
         const Self = @This();
