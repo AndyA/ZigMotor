f390efa08c8625a090441adb27701c016f111226
Andy Armstrong <andy@hexten.net>
2026-04-21 14:08:41 +0100

    Void callback

--- src/runtime/callback.zig
+++ src/runtime/callback.zig
@@ -1,4 +1,16 @@
 pub fn makeCallback(comptime PayloadType: type) type {
+    if (@sizeOf(PayloadType) == 0) {
+        return struct {
+            const Self = @This();
+            context: *anyopaque,
+            handler: *const fn (ctx: *anyopaque) anyerror!void,
+
+            pub fn advise(self: Self) !void {
+                try self.handler(self.context);
+            }
+        };
+    }
+
     return struct {
         const Self = @This();
         context: *anyopaque,
